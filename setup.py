dcb2f173467eb364aa9ef840d238b96c43573d07
Filipi565 <filipi565@hotmail.com>
2025-04-17 10:49:41 -0300

    Add a setup script

---
+++ setup.py
@@ -0,0 +1,8 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name="Numeros Loteria",
+    version="1.0.0",
+    description="github.com/Filipi565",
+    executables=[Executable("main.py", base="gui")],
+)
